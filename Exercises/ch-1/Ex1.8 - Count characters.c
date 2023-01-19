df81590fe1f5988a9d1aa53f2aecf5354a599e30
Syd <32424385+sprkbrn@users.noreply.github.com>
2023-01-18 22:10:19 -0500

    initial commit

---
+++ Exercises/ch-1/Ex1.8 - Count characters.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+const char EOL = '\0';
+
+struct word_stats {
+    int chars;
+    int spaces;
+} stats;
+
+void check_word (char* word) {
+    int char_count  =   0;
+    int space_count =   0;
+    
+    //  loop until end of line '\0'
+    for (int index = 0; word[index] != EOL; index++) {
+        
+        //  count spaces
+        if(word[index] == ' ') {
+            space_count++;
+        }
+
+        //  count chars
+        char_count++;
+    }
+
+    //  save the values to stats
+    stats.chars = char_count;
+    stats.spaces = space_count;
+}
+
+int main(int argc, char *argv[] ) {
+    char   user_input[50]   =   "";
+
+    //  concat all 
+    int     concat_index    =   1;
+    do
+    {
+        strcat_s(user_input, 50, argv[concat_index]);
+    } while (concat_index++ < (argc - 1));  // if the index is less than the arg count
+    
+    check_word(user_input);
+
+    printf("%s is %d characters long with %d spaces.", user_input, stats.chars, stats.spaces);
+}
